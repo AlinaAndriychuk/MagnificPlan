4117e67797c9a7c83594f8606e7dd23bf870816a
AlinaAndriychuk <alinka.andriychuk2002@gmail.com>
2020-10-04 22:22:21 +0300

    build react progect

---
+++ public/build/precache-manifest.c5d8ee11475f6554bd3cb9d22af77649.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6fcaf89b0912226d0b8fa9a3302b81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "114586f6a17838302926",
+    "url": "/static/css/main.9bcce356.chunk.css"
+  },
+  {
+    "revision": "28ef526cbb72f6ddbe1d",
+    "url": "/static/js/2.d050b4de.chunk.js"
+  },
+  {
+    "revision": "3377ffda74687e7745ec38a7bdbe5cdb",
+    "url": "/static/js/2.d050b4de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "114586f6a17838302926",
+    "url": "/static/js/main.e839f075.chunk.js"
+  },
+  {
+    "revision": "797afd30eb378f627c61",
+    "url": "/static/js/runtime-main.d7d96907.js"
+  },
+  {
+    "revision": "5d7be6d4085f7ee1962fdb840367d8da",
+    "url": "/static/media/dragndrop.5d7be6d4.png"
+  },
+  {
+    "revision": "5e513b416c66b231852f017caa2558cd",
+    "url": "/static/media/gradientdark.5e513b41.jpg"
+  },
+  {
+    "revision": "d601de24211e40ded82173788f446c55",
+    "url": "/static/media/gradientlight.d601de24.jpg"
+  },
+  {
+    "revision": "b49b3e4edda9af7dfe763fc3e15cdefe",
+    "url": "/static/media/list.b49b3e4e.png"
+  },
+  {
+    "revision": "d6ff79728a5fd30927f6e3c7fdd4e208",
+    "url": "/static/media/palette.d6ff7972.png"
+  }
+]);
